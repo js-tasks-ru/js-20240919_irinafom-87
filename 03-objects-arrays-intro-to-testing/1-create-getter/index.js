ef950c48723e1585fc3e3215969f3c9fe2ab64aa
Irina <irinafom_87@mail.ru>
2024-09-28 22:49:24 +0300

    Adding third lesson tasks' solutions

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -1,8 +1,19 @@
 /**
  * createGetter - creates function getter which allows select value from object
- * @param {string} path - the strings path separated by dot
+ * @param {string} fieldsString - the strings fieldsString separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+export function createGetter(fieldsString) {
 
+  return function(targetObject) {
+    const fields = fieldsString.split('.');
+
+    for (const field of fields) {
+      if (typeof targetObject !== 'object' || !targetObject.hasOwnProperty(field)) {
+        return undefined;
+      }
+      targetObject = targetObject[field];
+    }
+    return targetObject;
+  };
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -1,9 +1,20 @@
 /**
  * trimSymbols - removes consecutive identical symbols if they quantity bigger that size
- * @param {string} string - the initial string
+ * @param {string} sourceString - the initial string
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
-
+export function trimSymbols(sourceString, size) {
+  if (size === undefined) {
+    return sourceString;
+  }
+  const charsArray = sourceString.split('').map((char, index, arr) => {
+    if (char !== arr[index + 1]) {
+      char += '.';
+    }
+    return char;
+  });
+  const partsArray = charsArray.join('').slice(0, -1).split('.');
+  const resultArray = partsArray.map(part => part.length <= size ? part : part.slice(0, size));
+  return resultArray.join('');
 }
