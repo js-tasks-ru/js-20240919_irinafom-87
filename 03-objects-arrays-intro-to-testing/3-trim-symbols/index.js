25f489fb4c6d5d0182a858fbe91219013c593057
Irina <irinafom_87@mail.ru>
2024-09-29 14:16:40 +0300

    Adding variant of trimSymbols function's solution with string methods only

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -1,11 +1,29 @@
 /**
  * trimSymbols - removes consecutive identical symbols if they quantity bigger that size
  * @param {string} sourceString - the initial string
- * @param {number} size - the allowed size of consecutive identical symbols
+ * @param {number} maxChars - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(sourceString, size) {
-  if (size === undefined) {
+export function trimSymbols(sourceString, maxChars) {
+  if (maxChars === undefined) {
+    return sourceString;
+  }
+  let count = 0;
+  let resultString = '';
+  let size = 0;
+  for (let i = 0; i < sourceString.length; i++) {
+    count++;
+    if (sourceString[i] !== sourceString[i + 1]) {
+      size = Math.min(maxChars, count);
+      resultString += sourceString[i].repeat(size);
+      count = 0;
+    }
+  }
+  return resultString;
+}
+
+export function trimSymbolsVariant(sourceString, maxChars) {
+  if (maxChars === undefined) {
     return sourceString;
   }
   const charsArray = sourceString.split('').map((char, index, arr) => {
@@ -15,6 +33,6 @@ export function trimSymbols(sourceString, size) {
     return char;
   });
   const partsArray = charsArray.join('').slice(0, -1).split('.');
-  const resultArray = partsArray.map(part => part.length <= size ? part : part.slice(0, size));
+  const resultArray = partsArray.map(part => part.length <= maxChars ? part : part.slice(0, maxChars));
   return resultArray.join('');
 }
