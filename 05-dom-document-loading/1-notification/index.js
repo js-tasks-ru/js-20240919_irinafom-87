99bfc6468f29912759c3f8adbae2ef92b1da3411
Irina <irinafom_87@mail.ru>
2024-10-08 11:53:21 +0300

    Adding fifth task solution

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+  element;
 
+  constructor(title = '', props = {}) {
+    const {
+      duration = 0,
+      type = '',
+    } = props;
+    this.duration = duration;
+    this.type = type;
+
+    this.title = title;
+
+    this.element = this.show();
+  }
+
+  show(targetElement) {
+    const element = targetElement || document.createElement('div');
+    
+    element.innerHTML = this.createTemplate();
+
+    const firstElement = element.firstElementChild;
+
+    if (this.type) {
+      firstElement.classList.add(this.type);
+    }
+
+    setTimeout(() => this.remove(), this.duration);
+
+    return firstElement;
+  }
+
+  createTemplate() {
+    return `
+      <div class="notification" style="--value:20s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+            ${this.title}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,96 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  element;
+  subElements = {};
 
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.fieldsFromHeader = this.headerConfig.map(object => object.id);
+    this.element = this.createElement();
+
+    this.selectSubElements(false);
   }
+
+  selectSubElements() {
+    this.element.querySelectorAll('[data-element]').forEach(item => {
+      this.subElements[item.dataset.element] = item;
+    });
+  }
+
+  sort(fieldValue, orderValue) {
+    const locales = ['ru', 'en'];
+    const options = { sensitivity: 'variant', caseFirst: 'upper', numeric: true };
+    const collator = new Intl.Collator(locales, options);
+    const sortDesc = (a, b) => collator.compare(b[fieldValue], a[fieldValue]);
+    const sortAsc = (a, b) => collator.compare(a[fieldValue], b[fieldValue]);
+    const sortFunction = orderValue === 'asc' ? sortAsc : sortDesc;
+
+    this.data.sort(sortFunction);
+    this.element.innerHTML = this.createTemplate();
+
+    this.selectSubElements();
+  }
+
+  createElement() {
+    const div = document.createElement('div');
+
+    div.innerHTML = this.createTemplate();
+
+    return div.firstElementChild;
+  }
+
+  createHeaderColumns() {
+    let headerColumns = '';
+    for (const headerColumnData of this.headerConfig) {
+      headerColumns += `
+      <div class="sortable-table__cell" data-id="${headerColumnData.id}" data-sortable="${headerColumnData.sortable}">
+        <span>${headerColumnData.title}</span>
+      </div>
+    `;
+    }
+    return headerColumns;
+  }
+
+  createRows() {
+    let row = '';
+    for (const item of this.data) {
+      let cells = '';
+      for (const field of this.fieldsFromHeader) {
+        cells += `<div class="sortable-table__cell">${item[field]}</div>`;
+      }
+      row += `
+        <a href="/" class="sortable-table__row">
+          ${cells}
+        </a>
+      `;
+    }
+
+    return row;
+  }
+
+  createTemplate() {
+    return `
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+
+          <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.createHeaderColumns()}
+          </div>
+
+          <div data-element="body" class="sortable-table__body">
+            ${this.createRows()}
+          </div>
+        </div>
+    `;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
 }
 
