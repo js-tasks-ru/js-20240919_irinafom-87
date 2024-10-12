6fc0ae3206699198543f0cce0cacbc7a0d07c4b3
Irina <irinafom_87@mail.ru>
2024-10-12 12:59:00 +0300

    Adding 6.2 task, editing 6.1 and 5.2 task

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,65 @@
 class Tooltip {
-  initialize () {
+  element;
+  static instance;
+  static tipOffset = { x: 10, y: 10 }
 
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+    Tooltip.instance = this;
+    this.element = this.createElement(this.createTemplate(''));
+  }
+
+  initialize() {
+    document.addEventListener('pointermove', (e) => this.handlePointerMove(e));
+    document.addEventListener('pointerover', (e) => this.handlePointerOver(e));
+    document.addEventListener('pointerout', (e) => this.handlePointerOut(e));
+  }
+
+  handlePointerMove(event) {
+    if (this.element) {
+      this.element.style.left = `${event.target.clientX + Tooltip.tipOffset.x}px`;
+      this.element.style.top = `${event.target.clientY + Tooltip.tipOffset.y}px`;
+    }
+  }
+
+  handlePointerOver(event) {
+    const target = event.target.closest('[data-tooltip]');
+    if (!target) {
+      return;
+    }
+    this.render(target.dataset.tooltip);
+  }
+
+  render(tip) {
+    this.element.innerHTML = tip;
+    document.body.append(this.element);
+  }
+
+  handlePointerOut() {
+    this.remove();
+  }
+
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  } 
+
+  createTemplate(tip) {
+    return `<div class="tooltip">${tip}</div>`;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    document.removeEventListener('pointermove', () => this.handlePointerMove());
+    document.removeEventListener('pointerover', () => this.handlePointerOver());
+    document.removeEventListener('pointerout', () => this.handlePointerOut());
   }
 }
 
