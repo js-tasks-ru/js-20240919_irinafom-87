a8c84a527541c9eca30919d82e32910a80bb6b53
Irina <irinafom_87@mail.ru>
2024-10-21 15:13:59 +0300

    Adding 9.1 task

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -79,12 +79,20 @@ export default class ProductForm {
   }
 
   fillImagesData() {
-    const imageContainer = this.subElements.imageListContainer.firstElementChild;
+    const imageContainer = this.subElements.imageListContainer;
+    imageContainer.append(this.createList());
+  }
+
+  createList() {
+    const ul = document.createElement('ul');
+    ul.classList.add('sortable-list');
+
     for (const image of this.data.images) {
       const div = document.createElement('div');
       div.innerHTML = this.createImageTemplate(image.url, image.source);
-      imageContainer.append(div.firstElementChild);
+      ul.append(div.firstElementChild);
     }
+    return ul;
   }
 
   createImageTemplate(url, source) {
@@ -159,8 +167,6 @@ export default class ProductForm {
           <div class="form-group form-group__wide" data-element="sortable-list-container">
             <label class="form-label">Фото</label>
             <div data-element="imageListContainer">
-              <ul class="sortable-list">
-              </ul>
             </div>
             <button type="button" name="uploadImage" class="button-primary-outline"><span>Загрузить</span></button>
           </div>
--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,16 +1,33 @@
-import SortableList from '../../2-sortable-list/src/index.js';
+import SortableList from '../2-sortable-list/index.js';
+import ProductForm from '../../08-forms-fetch-api-part-2/1-product-form-v1/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
 
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ProductForm {
+export default class ProductFormV2 extends ProductForm {
   constructor (productId) {
+    super(productId);
     this.productId = productId;
   }
 
   async render () {
+    return await super.render();
+  }
 
+  createList() {
+    return new SortableList({ items: [...this.createItems()] }).element;
+  }
+
+  createItems() {
+    let items = [];
+    for (const image of this.data.images) {
+      const div = document.createElement('div');
+      div.innerHTML = this.createImageTemplate(image.url, image.source);
+      items.push(div.firstElementChild);
+    }
+
+    return items;
   }
 }
